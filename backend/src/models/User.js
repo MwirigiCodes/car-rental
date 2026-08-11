81b518bf41d8743209cc2b6caa7105ace920fba0
Cyber Coder <dmwirigi005@gmail.com>
2026-08-11 10:43:04 +0300

    add user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    cars: { type: Array },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
