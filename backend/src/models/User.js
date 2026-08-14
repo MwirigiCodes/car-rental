74696178e5e866c5dd333b576175eb1d83b3d0a5
Cyber Coder <dmwirigi005@gmail.com>
2026-08-14 14:40:59 +0300

    add a password field in user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     cars: { type: Array },
+    password: { type: String, required: true },
   },
   { timestamps: true },
 );
