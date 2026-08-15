08d2a3822e7c4e43d25b4b4b422adefb9bac9bfb
Cyber Coder <dmwirigi005@gmail.com>
2026-08-15 21:01:32 +0300

    import and use cookie-parser middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-// import cookieParser from 'cookie-parser'; // TODO: install cookie-parser package
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
 
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +13,11 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   connectDB();
